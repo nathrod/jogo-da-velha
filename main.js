f8a2d57fbfbb98c97e53a1c1f28bbf967f45257c
Nathalia Rodrigues <nathalia.rsv@outlook.com>
2023-09-20 20:28:41 -0300

    primeir versão

--- main.js
+++ main.js
@@ -1,12 +1,36 @@
 'use strict'
 
-let botao = document.getElementById('btnLight')
-let casas = document.querySelectorAll('.casa')
+let botao = document.getElementById('btnLight');
+let casas = document.querySelectorAll('.casa');
 
 //Evento para alterar entre modo escuro/claro
 botao.addEventListener('click', ()=>{
-    botao.classList.toggle('dark')
-    casas.forEach(casa => casa.classList.toggle('dark'))
+    botao.classList.toggle('dark');
+    casas.forEach(casa => casa.classList.toggle('dark'));
 })
 
-let jogador1 = document.getElementById('')
+//Mostrar tabuleiro
+
+let jogar = document.getElementById('jogar');
+let container = document.querySelectorAll('.container');
+let principal = document.querySelectorAll('.principal');
+let jogadores = document.querySelectorAll('.jogadores');
+
+jogar.addEventListener('click', ()=> {
+    let jogador1 = document.getElementById('f-name').value;
+    let jogador2 = document.getElementById('s-name').value;
+
+    //Só deve aparecer se o usuário digitar um nome, caso contrário
+    //emitir um alerta digite o nome dos jogadores
+
+    if(jogador1==="" && jogador2===""){
+        alert("Oops! Ainda não sei seus nomes 😔");
+    }else{
+        //Mostra a tela de jogo e esconde a outras divs
+        jogar.classList.toggle('show');
+        container.forEach(container => container.classList.toggle('show'));
+        principal.forEach(principal => principal.classList.toggle('show'));
+        jogadores.forEach(jogadores => jogadores.classList.toggle('show'));
+    }
+})
+
